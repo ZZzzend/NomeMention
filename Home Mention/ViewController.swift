b47ab34918234f63b7372efa10926495f8318df1
Владислав <Vladislav@MacBook-Pro-Temp.local>
2019-09-05 14:09:03 +0800

    start project

---
+++ Home Mention/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home Mention
+//
+//  Created by Владислав on 05/09/2019.
+//  Copyright © 2019 Vladislav Samelchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

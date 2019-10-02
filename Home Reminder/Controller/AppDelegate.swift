24a878dd6a36cf950abcdb397f2f81628f29c581
Владислав <Vladislav@MacBook-Pro-Temp.local>
2019-10-02 14:28:47 +0800

    added localized

--- Home Reminder/Controller/AppDelegate.swift
+++ Home Reminder/Controller/AppDelegate.swift
@@ -14,14 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     let notificationCenter = UNUserNotificationCenter.current()
-
     
-//    let mainViewController = MainViewController()
-//    init(mainViewController: MainViewController = MainViewController()) {
-//        self.MainViewController = mainViewController
-//    }
-    
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         requestAutorisation()
@@ -44,49 +37,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 }
-
-//extension AppDelegate: UNUserNotificationCenterDelegate {
-//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-//        completionHandler([.alert, .sound])
-//    }
-//
-//    func userNotificationCenter(_ center: UNUserNotificationCenter,
-//    didReceive response: UNNotificationResponse,
-//    withCompletionHandler completionHandler:
-//      @escaping () -> Void) {
-
-//        switch response.actionIdentifier {
-//          case "Snooze":
-//            // let mainViewController = UIApplication.shared.delegate as! MainViewController
-//            content.body = "Opps"
-//            let triggerSnooze = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-//            let request = UNNotificationRequest(identifier: identifire,
-//                                                content: content,
-//                                                trigger: triggerSnooze)
-//            notificationCenter.add(request) { (error) in
-//            if let errorSnooze = error {
-//                print("Error \(errorSnooze.localizedDescription)")
-//            }
-//            }
-//            print("Snooze")
-//
-//
-//          case "SnoozeSecond":
-//
-//              break
-//
-//          case "SnoozeThird":
-//
-//              break
-//
-//          case "Delete":
-//
-//              break
-//
-//          default:
-//             break
-//          }
-//
-//           completionHandler()
-//        }
-//}
